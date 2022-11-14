bd19df7077f158f0f01bbf5500d8ad593e4b088a
kymdennis3 <kymdennis3@gmail.com>
2022-11-14 23:06:15 +0300

    polishing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void  main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        char [] arr = text.toCharArray();
+        String rev ="";
+        for (char i : arr)
+            rev =1 + rev;
+        System.out.println(text + "this is Print backwards" + "Man! I'm becoming good at this. Holla!!");
     }
 }
